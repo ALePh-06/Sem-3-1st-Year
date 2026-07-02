c0a7c6dc0f316c7d7cb6377f621f091929162e14
K-Rizmi <01446muimpostmate@gmail.com>
2026-07-02 22:04:10 +0800

    Added CPU, call me if there's any needed changes, as I thought CPU should be adding much more stuff

--- OOPDS/Core.cpp
+++ OOPDS/Core.cpp
@@ -131,10 +131,37 @@ class StackIndex {
     }
 };
 
-class CPU () {
+class CPU {
+    private:
+    Registers reg;
+    Memory mem;
+    Flags flags;
+    ProgramCounter PC;
+    customStack CS;
+    
+    public:
 
-}
+    CPU () {};
+    // Registers focus
+    void setReg( int pos, int value) { reg.setRegister(pos, value); };
+    int8_t getReg(int pos) { return reg.getRegister(pos); };
+
+    // Memory focus
+    void setMem (int pos, int value) { mem.setMemory(pos, value); };
+    int8_t getMem( int pos ) { return mem.getMemory(pos); };
+
+    // PC focus
+    int8_t getPC() { return PC.getPC(); };
+    void incrementPC() { PC.IncPC(); };
+    
+    // Flags focus
+    bool getUF () { return flags.getUF(); };
+    bool getOF () { return flags.getOF(); };
+    bool getZF () { return flags.getZF(); };
+    bool getCF () { return flags.getCF(); };
+    
+};
+
 int main(){
-
     return 0;
 }
