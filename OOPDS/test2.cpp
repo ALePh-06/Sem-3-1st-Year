408d23459131a99ce0cec9bbeb378da36250c6f3
K-Rizmi <01446muimpostmate@gmail.com>
2026-07-03 19:51:14 +0800

    Finalised command input

--- OOPDS/test2.cpp
+++ OOPDS/test2.cpp
@@ -679,18 +679,19 @@ int Helper::resolveValueReg(string operand, CPU& cpu)
     if (operand.find('R') != -1)
     {
         int reg = getRegisterNumber(operand);
-        return cpu.getReg(reg);
+        return reg;
     }
 
     return stringToInt(operand);
 }
 
 int Helper::resolveAddressReg(string operand, CPU& cpu)
-{
+{   
     if (operand.find('R') != -1)
     {
         int reg = getRegisterNumber(operand);
-        return cpu.getReg(reg);
+        int val = cpu.getReg(reg);
+        return val;
     }
 
     return stringToInt(operand);
@@ -781,6 +782,9 @@ Instruction* Parser::parse(string line, CPU& cpu)
     string op1 = getFirstOperand(line);
     string op2 = getSecondOperand(line);
 
+    if (cmd == "RESET")
+        return new ResetFlag(op1);
+
     int dest = getRegisterNumber(op1);
     int src = getRegisterNumber(op2);
 
@@ -805,6 +809,23 @@ Instruction* Parser::parse(string line, CPU& cpu)
         return new Store(value, address);
     }
 
+    if (cmd == "SHL")
+        return new ShLIns(dest, src);
+
+    if (cmd == "SHR")
+        return new ShRIns(dest, src);
+
+    if (cmd == "ROL")
+        return new RoLIns(dest, src);
+
+    if (cmd == "ROR")
+        return new RoRIns(dest, src);
+
+    if (cmd == "PUSH")
+        return new Push(dest);
+
+    if (cmd == "POP")
+        return new Pop(dest);
 
     if (cmd == "ADD")
         return new Add(dest, src);
@@ -988,10 +1009,10 @@ int main()
     string inputFile;
     string outputFile;
 
-    cout << "Enter input assembly file (.asm): ";
+    cout << "Enter input assembly file in format (.asm): ";
     cin >> inputFile;
 
-    cout << "Enter output result file: ";
+    cout << "Enter output result file in format (.asm): ";
     cin >> outputFile;
 
     Runner runner(inputFile, outputFile);
