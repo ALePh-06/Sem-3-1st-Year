a54c0a66bbe2fd0bb651d5a33f9bdcbe81fc7874
K-Rizmi <01446muimpostmate@gmail.com>
2026-07-03 23:01:08 +0800

    added comments

--- OOPDS/test2.cpp
+++ OOPDS/test2.cpp
@@ -106,15 +106,17 @@ class customQueue
     int getSize() { return size; }
 };
 
+// Start Muiz part //
+// ========================================================================================================= //
 class Registers {
 
     private:
-    int8_t R[8];
+    int8_t R[8]; // making 8 spaces in an array
 
     public:
     Registers () 
     {
-        for ( int i = 0; i < 8; i++ )
+        for ( int i = 0; i < 8; i++ ) // initialise every registers to have value '0'
             R[i] = 0;
     }
 
@@ -123,19 +125,19 @@ class Registers {
     }
 
     void setRegister (int pos, int value) {
-        R[pos] = value;
+        R[pos] = value;             // setter to insert value to certain registers.
     } 
 };
 
 class Memory {
     private:
-    int8_t M[64];
+    int8_t M[64];  // to make 64 spaces in an array, signed 1 byte
 
     public:
     Memory ()
     {
         for ( int i = 0; i < 64; i++)
-        M[i] = 0;
+        M[i] = 0;        // initialise everything to have value '0'
     }
 
     int8_t getMemory ( int pos ) {
@@ -143,14 +145,14 @@ class Memory {
     }
 
     void setMemory ( int pos, int value ) {
-        M[pos] = value;
+        M[pos] = value;     // to insert value/input into memory allocations
     }
 
 };
 
 class Flags {
     private:
-    bool UF = false, OF = false, ZF = false, CF = false;
+    bool UF = false, OF = false, ZF = false, CF = false;  
 
     public:
 
@@ -237,7 +239,7 @@ class StackIndex {
     }
 };
 
-class CPU {
+class CPU {         // to initialise and combine every core classes into one
     private:
     Registers reg;
     Memory mem;
@@ -247,9 +249,9 @@ class CPU {
     
     public:
 
-    CPU () {};
+    CPU () {};          // constructor to initialise for other classes to use.
     // Registers focus
-    void setReg( int pos, int value) { reg.setRegister(pos, value); };
+    void setReg( int pos, int value) { reg.setRegister(pos, value); };      
     int8_t getReg(int pos) { return reg.getRegister(pos); };
 
     // Memory focus
@@ -276,6 +278,9 @@ class CPU {
     int8_t popStack()            { return CS.pop(); }
     int getSI()                  { return CS.getSI(); }
 };
+
+// End Muiz part //
+// ================================================================================================= //
 
 //ALif
 class Instruction {
@@ -828,7 +833,6 @@ Instruction* Parser::parse(string line, CPU& cpu)
     if (cmd == "DEC") return new Dsc(dest);
     if (cmd == "DISPLAY") return new Display(dest);
     if (cmd == "INPUT") return new Input(dest);
-    
     return nullptr;
 }
 
